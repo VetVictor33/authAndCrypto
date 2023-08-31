d111f63bb2411fb116b92b71cccecaecf1806cd4
Victor Feliciano <victorjfeliciano@gmail.com>
2023-08-31 09:50:35 -0300

    using static class for Router

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import HandleError from "./middlewares/HandleError";
 const app = express();
 
 app.use(express.json());
-app.use(new Routes().router);
+app.use(Routes.routes());
 app.use(HandleError)
 
 const port = process.env.PORT || 3000
--- src/routes.ts
+++ src/routes.ts
@@ -5,21 +5,16 @@ import AccountController from './controllers/AccountController';
 import TokenValidation from './middlewares/TokenValidation';
 import schemaValidation from './middlewares/schemaValidation';
 import { accountLoginSchema, accountSignupSchema, monsterCreationSchema } from './schemas/Schemas';
-export default class Routes {
-  public router: Router
+export default abstract class Routes {
+  private static router: Router = Router()
 
-  private monsterValidation: MonsterValidation = new MonsterValidation()
-  private tokenValidation: TokenValidation = new TokenValidation()
+  private static monsterValidation: MonsterValidation = new MonsterValidation()
+  private static tokenValidation: TokenValidation = new TokenValidation()
 
-  private accountController: AccountController = new AccountController()
-  private monsterController: MonsterController = new MonsterController()
+  private static accountController: AccountController = new AccountController()
+  private static monsterController: MonsterController = new MonsterController()
 
-  constructor() {
-    this.router = Router()
-    this.createRoutes()
-  }
-
-  private createRoutes() {
+  public static routes() {
     this.router.get('/', (req, res) => { res.json('Server is up and running') });
 
     this.router.post('/signup', schemaValidation(accountSignupSchema), this.accountController.signup);
@@ -32,5 +27,6 @@ export default class Routes {
     this.router.get('/monsters/:monsterId', this.monsterController.get);
     this.router.patch('/monsters/:monsterId/:field', this.monsterValidation.update, this.monsterController.patch);
     this.router.delete('/monsters/:monsterId', this.monsterController.delete)
+    return this.router
   }
 }

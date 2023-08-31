19b07a075849dca4ccc81c58e823dad61c927d19
Victor Feliciano <victorjfeliciano@gmail.com>
2023-08-31 11:44:04 -0300

    initial commit

--- src/middlewares/HandleError.ts
+++ src/middlewares/HandleError.ts
@@ -5,6 +5,5 @@ import NotFoundError from "../errors/NotFoundError";
 export default async function HandleError(err: Error & Partial<NotFoundError>, req: Request, res: Response, next: NextFunction) {
   const status = err.status ?? 500
   const message = err.status ? err.message : internalServerError
-
   return res.status(status).json({ message })
 }

cc9449c027fa08ae71532282803774a8f0f65b09
Victor Feliciano <victorjfeliciano@gmail.com>
2023-08-31 10:30:39 -0300

    error handler middleware is now async

--- src/middlewares/HandleError.ts
+++ src/middlewares/HandleError.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { internalServerError } from "../utils/MessageUtils";
 import NotFoundError from "../errors/NotFoundError";
 
-export default function HandleError(err: Error & Partial<NotFoundError>, req: Request, res: Response, next: NextFunction) {
+export default async function HandleError(err: Error & Partial<NotFoundError>, req: Request, res: Response, next: NextFunction) {
   const status = err.status ?? 500
   const message = err.status ? err.message : internalServerError
 
